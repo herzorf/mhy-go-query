a454ed46bb6d23d9bc246782f177b63bd7e60a5b
1446450047 <1446450047@qq.com>
2022-12-21 18:33:12 +0800

    成功爬到东西

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"github.com/chromedp/chromedp"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"strconv"
 	"strings"
 )
 
@@ -24,20 +29,57 @@ func ExampleScrape() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(res)
+	var num = 1
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(res))
 	doc.Find(".mhy-article-card .mhy-article-card__preview div").Each(func(i int, selection *goquery.Selection) {
 		large, existsLarge := selection.Attr("large")
 		dataSrc, existsDataSrc := selection.Attr("data-src")
 		if existsLarge {
-			fmt.Printf("large: %s\n", large)
-		}
-		if existsDataSrc {
-			fmt.Printf("large: %s\n", dataSrc)
+			err := downloadFile(large, strconv.Itoa(num)+".jpg")
+			if err != nil {
+				fmt.Println("download err ", err)
+			} else {
+				fmt.Println("large下载成功", num)
+			}
+			num = num + 1
+		} else if existsDataSrc {
+			err := downloadFile(dataSrc, strconv.Itoa(num)+".jpg")
+			if err != nil {
+				fmt.Println("download err ", err)
+			} else {
+				fmt.Println("datasrc下载成功", num)
+			}
+			num = num + 1
 		}
 	})
 }
 
+func downloadFile(URL, fileName string) error {
+	//Get the response bytes from the url
+	response, err := http.Get(URL)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return errors.New("Received non 200 response code")
+	}
+	file, err := os.Create("./images/" + fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 	ExampleScrape()
+	//downloadFile("https://upload-bbs.miyoushe.com/upload/2022/12/20/75276539/c2aff9d62128e681b6a85b95775ad5c1_8462257324862820376.jpg", "name.jpg")
 }
